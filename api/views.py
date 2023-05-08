b200df82246254f925a72b74aa71a04307d0a32e
tmsnvl95 <tmsnvl95@naver.com>
2023-05-08 23:34:07 +0900

    url add

--- api/views.py
+++ api/views.py
@@ -4,6 +4,6 @@ from rest_framework import viewsets
 from django.contrib.auth.models import User
 from api.serializers import UserSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet): # modelviewset을 상속받았기 때문에 user에 대한 crud가능
     queryset = User.objects.all()
     serializer_class = UserSerializer
